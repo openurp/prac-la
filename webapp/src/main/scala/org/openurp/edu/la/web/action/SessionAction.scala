532ddf207af0eb783c5f4d530efc613e332a5d51
段体华 <duantihua@gmail.com>
2019-04-22 13:53:24 +0800

    add some action

---
+++ webapp/src/main/scala/org/openurp/edu/la/web/action/SessionAction.scala
@@ -0,0 +1,8 @@
+package org.openurp.edu.la.web.action
+
+import org.beangle.webmvc.entity.action.RestfulAction
+import org.openurp.edu.la.model.LaSession
+
+class SessionAction extends RestfulAction[LaSession]{
+  
+}
---
+++ webapp/src/main/scala/org/openurp/edu/la/web/action/VolunteerAction.scala
@@ -0,0 +1,10 @@
+package org.openurp.edu.la.web.action
+
+import org.beangle.webmvc.api.action.ActionSupport
+import org.beangle.webmvc.entity.action.EntityAction
+import org.openurp.edu.boot.web.ProjectSupport
+import org.openurp.edu.la.model.Volunteer
+
+class VolunteerAction extends ActionSupport with EntityAction[Volunteer] with ProjectSupport {
+
+}
